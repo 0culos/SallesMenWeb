377b80408f0f698db957cc063ee750a216aba8e1
0culos <danilopereira.contato@gmail.com>
2022-08-19 19:16:32 -0300

    MySQL adaptation and first migration

--- SallesMenWeb/Startup.cs
+++ SallesMenWeb/Startup.cs
@@ -37,7 +37,8 @@ namespace SallesMenWeb
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SallesMenWebContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SallesMenWebContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SallesMenWebContext"), builder =>
+                        builder.MigrationsAssembly("SallesMenWeb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
