d0b1b3e7f423db0d296fe74a0edef133b9574d53
0culos <danilopereira.contato@gmail.com>
2022-08-22 18:41:06 -0300

    Update seller and custom service exception

--- SallesMenWeb/Services/SellerService.cs
+++ SallesMenWeb/Services/SellerService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using SallesMenWeb.Services.Exceptions;
 
 namespace SallesMenWeb.Services
 {
@@ -30,11 +31,28 @@ namespace SallesMenWeb.Services
             return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
-        public void Remove (int id)
+        public void Remove(int id)
         {
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e) // exceção de nível de acesso a dados capturada para ser relançada como exceção de serviço para o controlador
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
