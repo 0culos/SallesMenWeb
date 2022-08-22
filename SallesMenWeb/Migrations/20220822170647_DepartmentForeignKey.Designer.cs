5a278c57a29e252949f918374e3821ab8e56b00c
0culos <danilopereira.contato@gmail.com>
2022-08-22 14:12:11 -0300

    Foreign key not null (referential integrity)

--- SallesMenWeb/Migrations/20220822170647_DepartmentForeignKey.Designer.cs
+++ SallesMenWeb/Migrations/20220822170647_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SallesMenWeb.Models;
 
 namespace SallesMenWeb.Migrations
 {
     [DbContext(typeof(SallesMenWebContext))]
-    partial class SallesMenWebContextModelSnapshot : ModelSnapshot
+    [Migration("20220822170647_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SallesMenWeb/Services/SellerService.cs
+++ SallesMenWeb/Services/SellerService.cs
@@ -22,6 +22,7 @@ namespace SallesMenWeb.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
