dd8251963e8a46913a69a96c89c46063b3736224
0culos <danilopereira.contato@gmail.com>
2022-08-22 18:07:22 -0300

    Seller details and eager loading

--- SallesMenWeb/Controllers/SellersController.cs
+++ SallesMenWeb/Controllers/SellersController.cs
@@ -60,10 +60,27 @@ namespace SallesMenWeb.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Delete (int id)
+        public IActionResult Delete(int id)
         {
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
         }
     }
 }
