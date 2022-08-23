b49c8c21c7699b0dd04159c51a89d232935e5d63
0culos <danilopereira.contato@gmail.com>
2022-08-23 17:50:56 -0300

    Asynchronous operations using Tasks (async, await)

--- SallesMenWeb/Services/DepartmentService.cs
+++ SallesMenWeb/Services/DepartmentService.cs
@@ -1,6 +1,8 @@
 ﻿using SallesMenWeb.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SallesMenWeb.Services
 {
@@ -13,9 +15,9 @@ namespace SallesMenWeb.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync() // sincronização assíncrona da função
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync(); // await usado para avisar o compilador que essa chamada será assíncrona
         }
 
     }
