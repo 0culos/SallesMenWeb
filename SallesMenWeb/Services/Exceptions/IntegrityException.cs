090117364bc0a33fb6de572dcd2b105d9f302086
0culos <danilopereira.contato@gmail.com>
2022-08-23 18:04:11 -0300

    Exception handling for delete (referential integrity)

---
+++ SallesMenWeb/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SallesMenWeb.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+        }
+    }
+}
