42f18aa508d0018c48ac0cde4d098a43fdaa7d4a
0culos <danilopereira.contato@gmail.com>
2022-08-23 19:06:11 -0300

    Implementing simple search

---
+++ SallesMenWeb/Services/SalesRecordService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SallesMenWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SallesMenWeb.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SallesMenWebContext _context;
+        public SalesRecordService(SallesMenWebContext context) // dependência para o context do framework
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result                                    // join das classes Vendedor e Departamento
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+}
