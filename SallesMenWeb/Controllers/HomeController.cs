bac8b4b7b07c9f9e295a9985b433a466dd4a23f5
0culos <danilopereira.contato@gmail.com>
2022-08-25 14:31:26 -0300

    Cleaning some features

--- SallesMenWeb/Controllers/HomeController.cs
+++ SallesMenWeb/Controllers/HomeController.cs
@@ -24,18 +24,6 @@ namespace SallesMenWeb.Controllers
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
